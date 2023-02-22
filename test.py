08969e3a7bf38e0f2ecd099a03ac35de8da0ed74
dasmksong <mksong@intellicode.co.kr>
2023-02-22 10:19:56 +0900

    refactor delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-from modules.preprocessing import drop_col, drop_na
-
-data = pd.read_csv("C:/Users/mkson/Downloads/data.csv", encoding="cp949")
-print(len(data))
-drop_na(data)
-print(len(data))
